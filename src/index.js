a2fa7d63f03e46900f3c64f67a3f019f54b99ab8
1codequeen <queenyona21@gmail.com>
2024-10-17 14:51:23 +0200

    added search engine on js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+}
+let searchInput = document.querySelector("#search-bar-input");
+let cityElement = document.querySelector("#city");
+cityElement.innerHTML = searchInput.value;
+
+let searchBarElement = document.querySelector("#search-bar");
+searchBarElement.addEventListener("submit", handleSearchSubmit);
